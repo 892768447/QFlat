e22f057ee5713ea41a24850dac330fd6d8bc980a
Irony <892768447@qq.com>
2018-07-09 00:54:35 +0800

    test

---
+++ Test/TestTooltip.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel
+
+from Widgets.ToolTip import ToolTip
+from PyQt5.QtCore import QObject, QRegExp
+from PyQt5.QtGui import QWindow
+
+
+# Created on 2018年7月8日
+# author: Irony
+# site: https://github.com/892768447
+# email: 892768447@qq.com
+# file: module
+# description: 
+__Author__ = """By: Irony
+QQ: 892768447
+Email: 892768447@qq.com"""
+__Copyright__ = 'Copyright (c) 2018 Irony'
+__Version__ = 1.0
+
+class Window(QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super(Window, self).__init__(*args, **kwargs)
+        layout = QVBoxLayout(self)
+        btn = QPushButton('按钮',self, clicked=self.onClick)
+        btn.setObjectName('haha')
+        btn.setToolTip('aaa')
+        layout.addWidget(btn)
+        
+    def onClick(self):
+        ToolTip.showText(self.sender(), 'text')
+
+if __name__ == '__main__':
+    import sys
+    from PyQt5.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+    w = Window()
+    w.show()
+    sys.exit(app.exec_())
+    
--- Widgets/ToolTip.py
+++ Widgets/ToolTip.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtWidgets import QLabel
 
 
@@ -27,11 +27,27 @@ class ToolTip(QLabel):
         self.setWindowFlags(Qt.ToolTip | Qt.BypassGraphicsProxyWidget)
 
     @classmethod
-    def showText(self, widget, text='', direction=2):
-        """显示文字"""
+    def showText(self, widget, text='', direction=1):
+        '''
+        # 显示文字
+        :param widget: 要显示文字提示的控件
+        :param text: 文字
+        :param direction: 方向 ( 左上右下 = 0, 1, 2, 3 )
+        '''
         if not ToolTip._instance:
             ToolTip._instance = ToolTip()
+        #设置文本内容
         ToolTip._instance.setText(text)
+        #得到坐标地址
         pos = widget.mapToGlobal(widget.pos()) - widget.pos()
+        if direction == self.Left:
+            pass
+        elif direction == self.Top:
+            pos = pos + QPoint(int((widget.width() - ToolTip._instance.width()) / 2), 
+                             - ToolTip._instance.height() - 10)
+        elif direction == self.Right:
+            pass
+        else:
+            pass
         ToolTip._instance.move(pos)
         ToolTip._instance.show()
